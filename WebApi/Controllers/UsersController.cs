6f94267ee57f5b19a028d55449cacf48c2e0171c
luoxiang <luoxiang@haisichen.com>
2018-05-16 21:44:18 +0800

    First Commit

---
+++ WebApi/Controllers/UsersController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Security;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using WebApi.Filter;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [HttpBasicAuthorize]
+    public class UsersController : ApiController
+    {
+        [AllowAnonymous]
+        [HttpPost]
+        public string AccountLogin()
+        {
+            string account = HttpContext.Current.Request.Form["account"];
+            string password= HttpContext.Current.Request.Form["password"];
+
+            if (account.Equals("admin") && password.Equals("123456"))
+            {
+                FormsAuthenticationTicket ticketObj = new FormsAuthenticationTicket(
+                    0, account, DateTime.Now, DateTime.Now.AddHours(1), true,
+                    $"{account}&{password}",
+                    FormsAuthentication.FormsCookiePath);
+                var result = new
+                {
+                    Result = true,
+                    Ticket = FormsAuthentication.Encrypt(ticketObj)
+                };
+
+                return JsonConvert.SerializeObject(result);
+            }
+            else
+            {
+                var result = new {Result = false};
+                return JsonConvert.SerializeObject(result);
+            }
+        }
+       
+
+        [HttpPost]
+        public Users Register(Users users)
+        {
+            Users _users = users;
+            return _users;
+        }
+
+
+        public string RegisterObject(JObject Jinfo)
+        {
+            dynamic jdata = Jinfo;
+            JObject jUser = jdata.User;
+            string info = jdata.info;
+            var user = jUser.ToObject<Users>();
+            return JsonConvert.SerializeObject(user);
+        }
+    }
+}
---
+++ WebApi/Filter/HttpBasicAuthorizeAttribute.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using System.Web.Http;
+using System.Web.Security;
+
+namespace WebApi.Filter
+{
+    public class HttpBasicAuthorizeAttribute : AuthorizeAttribute
+    {
+        public override void OnAuthorization(System.Web.Http.Controllers.HttpActionContext actionContext)
+        {
+            var authorizeation = actionContext.Request.Headers.Authorization;
+
+            if (actionContext.ActionDescriptor.GetCustomAttributes<AllowAnonymousAttribute>(true).Count != 0 ||
+                actionContext.ActionDescriptor.ControllerDescriptor.GetCustomAttributes<AllowAnonymousAttribute>(true)
+                    .Count != 0)
+            {
+                base.OnAuthorization(actionContext);
+            }
+            else if (authorizeation!=null&&authorizeation.Parameter!=null)
+            {
+                //用户验证
+                if (ValidateTicket(authorizeation.Parameter))
+                {
+                    base.IsAuthorized(actionContext);
+                }
+                else
+                {
+                    HandleUnauthorizedRequest(actionContext);
+                }
+            }
+            else
+            {
+                HandleUnauthorizedRequest(actionContext);
+            }
+        }
+
+        protected override void HandleUnauthorizedRequest(System.Web.Http.Controllers.HttpActionContext actionContext)
+        {
+            actionContext.Response = new HttpResponseMessage(System.Net.HttpStatusCode.Unauthorized)
+            {
+                ReasonPhrase = "Unauthorized"
+            };
+        }
+
+        private bool ValidateTicket(string encryptTicket)
+        {
+            var Ticket = FormsAuthentication.Decrypt(encryptTicket).UserData;
+            return string.Equals(Ticket, string.Format("{0}&{1}", "admin", "123456"));
+        }
+    }
+}
